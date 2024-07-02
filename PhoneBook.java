50fa6d964ec9bfdb52a86b990547ebf2cf083021
John Higgins <johnrhiggins94@gmail.com>
2024-07-02 11:19:23 -0400

    Add files via upload

---
+++ PhoneBook.java
@@ -0,0 +1,82 @@
+import java.util.*; 
+/*
+                                                     *Programming Challenge 17*           
+      This problem was weird. Challenge said to make five objects with the class, store them in an array in this program, then display information. 
+         Seems very poorly optimized and not able to scale. Should have made the Array List in the PhoneBookEntry class.  
+*/
+
+/**
+   The PhoneBook class creates five objects wih the PhoneBookEntry class o recieve user input, 
+   then stores the informaton from the five objects into an ArrayList, then displays data to user. 
+*/ 
+
+public class PhoneBook { 
+   public static void main(String[] args) { 
+      
+      Scanner scan = new Scanner(System.in); 
+      ArrayList<String> phonebook = new ArrayList<>(); 
+      final int NAME_PHONENUM_PAIR = 2; 
+      String name, phoneNum; 
+ 
+  
+      System.out.print("\nEnter Person's Name: "); 
+      name = scan.nextLine(); 
+      System.out.print("\nEnter " + name + "'s PhoneNumber: ");
+      phoneNum= scan.nextLine(); 
+      PhoneBookEntry phonebook1 = new PhoneBookEntry(name ,phoneNum);
+      phonebook.add(phonebook1.getName()); 
+      phonebook.add(phonebook1.getPhoneNumber());  
+      
+      System.out.print("\nEnter Person's Name: "); 
+      name = scan.nextLine(); 
+      System.out.print("\nEnter " + name + "'s PhoneNumber: ");
+      phoneNum= scan.nextLine(); 
+      PhoneBookEntry phonebook2 = new PhoneBookEntry(name ,phoneNum);
+      phonebook.add(phonebook2.getName()); 
+      phonebook.add(phonebook2.getPhoneNumber());
+      
+      System.out.print("\nEnter Person's Name: "); 
+      name = scan.nextLine(); 
+      System.out.print("\nEnter " + name + "'s PhoneNumber: ");
+      phoneNum= scan.nextLine(); 
+      PhoneBookEntry phonebook3 = new PhoneBookEntry(name ,phoneNum);
+      phonebook.add(phonebook3.getName()); 
+      phonebook.add(phonebook3.getPhoneNumber());
+      
+      System.out.print("\nEnter Person's Name: "); 
+      name = scan.nextLine(); 
+      System.out.print("\nEnter " + name + "'s PhoneNumber: ");
+      phoneNum= scan.nextLine(); 
+      PhoneBookEntry phonebook4 = new PhoneBookEntry(name ,phoneNum);
+      phonebook.add(phonebook4.getName()); 
+      phonebook.add(phonebook4.getPhoneNumber());
+      
+      System.out.print("\nEnter Person's Name: "); 
+      name = scan.nextLine(); 
+      System.out.print("\nEnter " + name + "'s PhoneNumber: ");
+      phoneNum= scan.nextLine(); 
+      PhoneBookEntry phonebook5 = new PhoneBookEntry(name ,phoneNum);
+      phonebook.add(phonebook5.getName()); 
+      phonebook.add(phonebook5.getPhoneNumber());
+      
+      int counter = -1; 
+      int size = phonebook.size()/ 2; 
+      
+      for (int index = 0; index < size; index++) { 
+         counter++; 
+         System.out.print("\nName: " + phonebook.get(counter)); 
+         counter++; 
+         System.out.print(" PhoneNumber: " + phonebook.get(counter)); 
+      }
+      
+   }
+} 
+            
+      
+      
+      
+      
+      
+       
+          
+         
---
+++ PhoneBookEntry.java
@@ -0,0 +1,116 @@
+/*
+
+                                                     *Programming Challenge 17*           
+      This problem was weird. Challenge said to make five objects with this class, store them in an Array List in the another program (PhoneBook.java in this case),
+      then display information there. Seems very poorly optimized and not able to scale. Should have made the Array List in this class to use one object. 
+*/ 
+
+/**
+   The PhoneBookEntry class holds information relating to a person's name and their phonenumber
+*/ 
+public class PhoneBookEntry { 
+
+   private String name; 
+   private String phoneNumber; 
+   
+  /**
+   Constructor 
+  */ 
+  
+   public PhoneBookEntry() { 
+      name = "Not Set"; 
+      phoneNumber = "Not Set"; 
+  }
+  
+   /**
+     Constructor
+     @perma person String for name 
+     @perma phoneNum String for phoneNumber
+   */ 
+   
+   public PhoneBookEntry (String person, String phoneNum) { 
+      name = person; 
+      phoneNumber = phoneNum;
+       
+   } 
+   
+   /**
+         Constructor
+     @perma person String for name 
+     @perma phoneNum Int for phoneNumber 
+   */ 
+   
+   public PhoneBookEntry (String person, int phoneNum) { 
+      name = person;
+      phoneNumber = String.valueOf(phoneNum); 
+   } 
+   
+   /**
+     Constructor
+     @perma person String for name 
+     @perma phoneNum Double for phoneNumber
+   */ 
+   
+   public PhoneBookEntry (String person, double phoneNum) { 
+      name = person;
+      phoneNumber = String.valueOf(phoneNum); 
+   } 
+   
+   /**
+      The setName method sets name field 
+      @perma The String for name variable
+   */ 
+   
+   public void setName(String person) { 
+      name = person; 
+   } 
+   
+   /**
+      The getName method returns name
+      @return name The name of the person
+   */ 
+   
+   public String getName() { 
+      return name; 
+   } 
+   
+   /**
+      The setPhoneNumber method sets the phonenumber field
+      @perma phoneNum The String for the phoneNumber
+   */ 
+   
+   public void setPhoneNumber(String phoneNum) { 
+      phoneNumber = phoneNum; 
+   } 
+   
+   /**
+      The setPhoneNumber method sets the phonenumber field
+      @perma phoneNum the Int for the phoneNumber
+   */ 
+   
+   public void setPhoneNumber (int phoneNum) { 
+      phoneNumber = String.valueOf(phoneNum); 
+   } 
+   
+   /**
+      The setPhoneNumber method sets the phonenumber field
+      @perma phoneNum the Double for the phoneNumber
+   */ 
+   
+   public void setPhoneNumber(double phoneNum) { 
+      phoneNumber = String.valueOf(phoneNum); 
+   } 
+   
+   /**
+      The getPhnoeNumber method return phoneNumber
+      @return phoneNumber The person's phoneNumber
+   */ 
+   
+   public String getPhoneNumber() { 
+      return phoneNumber; 
+  } 
+  
+} 
+
+   
+   
